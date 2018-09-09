d197822f9dd9c86d9751a9279470ed79e8d4b1ee
Jiří Filip <j.f.ilip@seznam.cz>
2018-09-09 16:01:27 +0200

    added multiprocessing for SLS training

--- train_model_SLS.py
+++ train_model_SLS.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+
+from multiprocessing import Pool
 from rule_miner import run_fim_apriori
 from IDS_smooth_local import (
     smooth_local_search,
@@ -11,24 +13,11 @@ from IDS_smooth_local import (
 
 
 
-df = pd.read_csv('data/iris0.csv', ',')
 
-df_raw = df.iloc[:, :-1]
-Y = df.iloc[:, -1]
+        
 
 
-rules = run_fim_apriori(df_raw, 0.25)
-
-
-
-list_of_rules = createrules(rules, list(set(Y)))
-prepare_caches(list_of_rules, df, Y)
-
-
-print("----------------------")
-for r in list_of_rules:
-    r.print_rule()
-
+"""
 lambda_array = [1.0]*7     # use separate hyperparamter search routine
 s1 = smooth_local_search(list_of_rules, df, Y, lambda_array, 0.33, 0.33)
 s2 = smooth_local_search(list_of_rules, df, Y, lambda_array, 0.33, -1.0)
@@ -42,10 +31,62 @@ if f1 > f2:
 else:
     print("The Solution Set is: "+str(s2))
     result_set = list(s2)
+"""
+
+df = pd.read_csv('data/iris0.csv', ',')
+df_raw = df.iloc[:, :-1]
+Y = df.iloc[:, -1]
+
+rules = run_fim_apriori(df_raw, 0.25)
+list_of_rules = createrules(rules, list(set(Y)))
+prepare_caches(list_of_rules, df, Y)
+
+def optimize(param):
+    lambda_array = [1.0]*7     # use separate hyperparamter search routine
+    s1 = smooth_local_search(list_of_rules, df, Y, lambda_array, 0.33, param)
+    f1 = func_evaluation(s1, list_of_rules, df, Y, lambda_array)
+
+    return f1, s1
+
+def f(x):
+    return x * x
+
+if __name__ == "__main__":
+   
+
+    # precompute values for cover and correct cover
+    
+
+    print("----------------------")
+    for r in list_of_rules:
+        r.print_rule()
+
+
+    
+
+    
+
+    process_pool = Pool(2)
+    results = process_pool.map(optimize, [0.33, -1.0])
+
+    #results = process_pool.map(f, [2, 3])
+
+    (f1, s1), (f2, s2) = results 
+
+    result_set = {}
+    if f1 > f2:
+        print("The Solution Set is: "+str(s1))
+        result_set = list(s1)
+    else:
+        print("The Solution Set is: "+str(s2))
+        result_set = list(s2)
 
 
 
-np_rules = np.array(list_of_rules)
-solution_rules = np_rules[result_set]
 
-list(map(lambda r: r.print_rule(), solution_rules))
+    np_rules = np.array(list_of_rules)
+    solution_rules = np_rules[result_set]
+
+    print("---------------------")
+    list(map(lambda r: r.print_rule(), solution_rules))
+    
